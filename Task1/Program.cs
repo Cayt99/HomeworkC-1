3fb818cd093be64dd13063f2fca815b5662a9991
Usmanov Saut <cayt99@mail.ru>
2023-06-23 14:00:25 +0600

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Объявление переменных
+        int a, b;
+
+        // Ввод первого числа
+        Console.Write("Введите первое число (a): ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        // Ввод второго числа
+        Console.Write("Введите второе число (b): ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        // Проверка на большее число и вывод результата
+        if(a > b) {
+            Console.WriteLine($"max = {a}, min = {b}");
+        } else if(b > a) {
+            Console.WriteLine($"max = {b}, min = {a}");
+        } else {
+            Console.WriteLine($"a = b = {a}");
+        }
+    }
+}
